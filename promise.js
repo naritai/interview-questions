6fde4deeb7f1a66a2a74809b590ba6442a5a077e
Naritai <alexandr.voronetskiy@gmail.com>
2020-10-12 22:17:12 +0300

    First commit

---
+++ promise.js
@@ -0,0 +1,104 @@
+
+
+class SimplePromise {
+  constructor(executionFunction) {
+    this.state = null;
+    this.result = null;
+    this.error = null;
+    this.errorHandlerSaved = false;
+    this.handlers = [];
+    this.executionFunction = executionFunction;
+
+    this.run();
+  }
+
+  run = () => {
+    this.state = 'pending';
+    this.executionFunction(this.resolve, this.reject);
+  }
+
+  resolve = (result) => {
+    this.state = 'fulfilled';
+    this.result = result;
+    this.handlers
+      .filter(cb => cb.type === 'fulfilled' || cb.type === 'finally')
+      .forEach(cb => cb.fn(this.result));
+  }
+
+  reject = (error) => {
+    this.state = 'rejected';
+    this.error = error;
+    this.handlers
+      .filter(cb => cb.type === 'rejected' || cb.type === 'finally')
+      .forEach(cb => cb.fn(this.error));
+  }
+
+  then = (resolveHandler) => {    
+    if (this.state === 'fulfilled') {
+      console.log('Promise has already been fulfilled!')
+    } else if (this.state === 'pending') {
+      this.handlers.push({ type: 'fulfilled', fn: resolveHandler });
+    }
+    return this;
+  }
+
+  catch = (errorHandler) => { 
+    // there is only one error handler can run!
+    if (this.state === 'rejected') {
+      console.log('Promise has already been rejected');
+    } else if (this.state === 'pending' && !this.errorHandlerSaved) {
+      this.handlers.push({ type: 'rejected', fn: errorHandler });
+      this.errorHandlerSaved = true;
+    }
+    return this;
+  }
+
+  finally = (fulfilledCallback) => {
+    // finally callbacks run always: before and after .then and .catch plus, with order save behaviour.
+    this.handlers.push({ type: 'finally', fn: fulfilledCallback }); 
+    return this;
+  }
+}
+
+const request = new SimplePromise((resolve, reject) => {
+  setTimeout(() => {
+    if (Math.random() < 0.5) {
+      resolve('success')
+    } else {
+      reject(new Error('Some error happened'));
+    }
+  }, 1000);
+});
+
+// TEST
+request.then(console.log)
+  .then(data => console.log('First .then + data :', data))
+  .finally(() => console.log('Finally before .catch'))
+  .catch(console.log)
+  .finally(() => console.log('This is a second finally!'))
+  .then(data => console.log('New .then after second finally!', data))
+  .finally(() => console.log('Third finally!'))
+  .catch(error => console.log("Should NOT see me, I'm the second .catch! ", error))
+  .finally(() => console.log("I'm the last finally after second .catch that you shouldn't see"))
+  .then(data => console.log('Last .then woth data :', data));
+
+
+// Real promises work
+// new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (Math.random() < 0.5) {
+//       reject(new Error('Some error'))
+//     } else {
+//       resolve('Happy result!');
+//     }
+//   }, 1000)
+// })
+// .then(console.log)
+// .finally(data => console.log('FINALLY :', data))
+// .then(data => {
+//     console.log('Another then');
+//     console.log('Data in new then :', data);
+// })
+// .catch(console.log)
+// .catch(error => console.log('Second error handler :', error))
+// .finally(() => console.log('Finally after last catch'))
